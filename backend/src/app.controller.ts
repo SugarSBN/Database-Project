245d124ba79937d1f6f09f27e3c0ee8ee473a7ae
fastle <fastle@163.com>
2023-05-21 13:30:09 +0800

    init

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Post, Put } from '@nestjs/common';
+import { AppService } from './app.service';
+
+// 主路径为 app
+@Controller("app")
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+  
+  // 1. 固定路径：
+  // 可以匹配到 get请求，http://localhost:9080/app/list
+  @Get("list")
+  getHello(): string {return "Hello"}
+  
+  // 可以匹配到 post请求，http://localhost:9080/app/list
+  @Post("list")
+  create():string{return "create"}
+  
+  // 2.通配符路径(?+* 三种通配符 )
+  // 可以匹配到 get请求, http://localhost:9080/app/user_xxx
+  @Get("user_*")
+  getUser(){return "getUser"}
+  
+  // 3.带参数路径
+  // 可以匹配到put请求，http://localhost:9080/app/list/xxxx
+  @Put("list/:id")
+  update(){ return "update"}
+}
+  
