1799dc9df0b5241f32e1d8eee098ebe7c8665539
fastle <fastle@163.com>
2023-05-26 20:35:58 +0800

    feat:delete some meaningless work

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService, ConfigModule } from '@nestjs/config';
 import envConfig from '../config/env';
 import { PostsEntity } from './posts/posts.entity';
-import { UserModule } from './user/user.module';
-import { RootModule } from './root/root.module';
-import { UsersEntity } from './root/entities/root.entity';
-import { StudentModule } from './student/student.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ 
@@ -21,7 +17,6 @@ import { StudentModule } from './student/student.module';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'mysql', 
-        entities: [PostsEntity,UsersEntity],  
         host: configService.get('DB_HOST', 'localhost'), 
         port: configService.get<number>('DB_PORT', 3306), 
         username: configService.get('DB_USER', 'root'),   
@@ -29,12 +24,10 @@ import { StudentModule } from './student/student.module';
         database: configService.get('DB_DATABASE', 'blog'), 
         timezone: '+08:00', 
         synchronize: true, 
+        autoLoadEntities: true,
       }),
     }),
     PostsModule,
-    UserModule,
-    RootModule,
-    StudentModule,
   ], 
   controllers: [AppController],
   providers: [AppService]
--- backend/src/root/dto/update-root.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateRootDto } from './create-root.dto';
-
-export class UpdateRootDto extends PartialType(CreateRootDto) {}
