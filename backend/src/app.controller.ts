3eb8837e16905bceef0807dcf50dc0c6d387624e
fastle <fastle@163.com>
2023-05-29 10:17:22 +0800

    fix: delete meaningless routes

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,23 +6,5 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
   
-  // 1. 固定路径：
-  // 可以匹配到 get请求，http://localhost:9080/app/list
-  @Get("list")
-  getHello(): string {return "Hello"}
-  
-  // 可以匹配到 post请求，http://localhost:9080/app/list
-  @Post("list")
-  create():string{return "create"}
-  
-  // 2.通配符路径(?+* 三种通配符 )
-  // 可以匹配到 get请求, http://localhost:9080/app/user_xxx
-  @Get("user_*")
-  getUser(){return "getUser"}
-  
-  // 3.带参数路径
-  // 可以匹配到put请求，http://localhost:9080/app/list/xxxx
-  @Put("list/:id")
-  update(){ return "update"}
 }
   
